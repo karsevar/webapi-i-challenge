028de42b38b8beff10dd7e02722ead848da98479
masonkarsevar <masonkarsevar@gmail.com>
2019-09-02 13:50:30 -0700

    application initialized. basic hobbit api coded

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// implement your API here
+
+const express = require('express');
+
+const server = express();
+// gives us an instance of a serve powered by express.
+
+// The next code snippets is the configuration of this server.
+
+server.get('/', (req, res) => {
+    res.send('Hello world');
+});
+
+// endpoint that returns a list of hobbits:
+server.get('/hobbits', (req, res) => {
+    const hobbits = [
+        {
+            id: 1, 
+            name: 'Samwise Gamgee'
+        },
+        {
+            id: 2,
+            name: 'Frodo Baggins'
+        }
+    ];
+
+    res.status(200).json(hobbits);
+})
+
+// A port that the client can listen to when accessing the web server.
+// A server can have more than one service running. Each service will have their own ports.
+// Emails are port 25 and apis for clients are port 80.
+
+// We can't use any these ports because we are privately developing an api.
+server.listen(3000, () => {
+    console.log('api running in port 3000') 
+})
